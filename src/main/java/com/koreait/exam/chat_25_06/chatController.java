18b06763aee2320d6803fd5c184f419000b55d26
yeji-star <sizuku134@gmail.com>
2025-06-11 15:59:15 +0900

    06.11

--- src/main/java/com/koreait/exam/chat_25_06/chatController.java
+++ src/main/java/com/koreait/exam/chat_25_06/chatController.java
@@ -3,15 +3,30 @@ package com.koreait.exam.chat_25_06;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/chat")
 public class chatController {
 
-    chatMessage message = new chatMessage("홍길동", "안녕");
+    private List<chatMessage> chatMessages = new ArrayList<>();
+
+    public record writeChatMessageResponse(long id) {
+
+    }
+
 
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<chatMessage> writeMessage() {
-        return new RsData("S-1", "메세지가 작성됨", message);
+    public RsData<writeChatMessageResponse> writeMessage() {
+        chatMessage message = new chatMessage("홍길동", "안녕");
+
+        chatMessages.add(message);
+
+        return new RsData<>(
+                "S-1",
+                "메세지가 작성됨",
+                new writeChatMessageResponse(message.getId()));
     }
 }
--- src/main/java/com/koreait/exam/chat_25_06/chatMessage.java
+++ src/main/java/com/koreait/exam/chat_25_06/chatMessage.java
@@ -14,6 +14,14 @@ public class chatMessage {
     private String content;
 
     public chatMessage(String authorName, String content) {
-        this(1, LocalDateTime.now(), authorName, content);
+        this(chatMessageIdGenerator.getNextId(), LocalDateTime.now(), authorName, content);
+    }
+}
+
+class chatMessageIdGenerator {
+    private static long id = 0;
+
+    public  static long getNextId() {
+        return ++id;
     }
 }
